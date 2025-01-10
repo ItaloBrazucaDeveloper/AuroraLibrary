3958bdb51766686867a9339c9637a71d0b07f9d0
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2025-01-10 10:00:47 -0300

    fix: Update useEffect of hook 'useTheme'

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -5,14 +5,14 @@ type StateThemeType = 'light' | 'dark' | 'system';
 export function useTheme() {
 	const [theme, setTheme] = useState<StateThemeType>('system');
 
-	const isDarkMode =
-		localStorage.AuroraTheme === 'dark' ||
-		(!('theme' in localStorage) &&
-			window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-	document.documentElement.classList.toggle('dark', isDarkMode);
-
 	useEffect(() => {
+		const isDarkMode =
+			localStorage.AuroraTheme === 'dark' ||
+			(!('theme' in localStorage) &&
+				window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+		document.documentElement.classList.toggle('dark', isDarkMode);
+
 		localStorage.AuroraTheme = theme;
 	}, [theme]);
 
